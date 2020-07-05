2ff667bdd785ff750d2d3bbf2c6dae8a7c2b1104
vigdorov <vigdorov@yandex-team.ru>
2020-07-05 20:13:18 +0300

    final init

--- src/app.js
+++ src/app.js
@@ -1,8 +1,3 @@
 import './app.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import iconTree from './images/icon-tree.png';
-
-const image = document.querySelector('#img');
-image.src = iconTree;
