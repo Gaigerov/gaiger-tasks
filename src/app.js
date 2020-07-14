9575c25a226e726195c30887a0df352dcb0c73e8
Gaigerov <gaigerov@gmail.com>
2020-07-14 11:31:26 +0300

    TS-3.Сделал тоггл и кнопка добавить с функцией

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
 import './app.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+var btn = document.querySelector('#btn');
+
+btn.onclick = function () {
+    var elemCopy = document.querySelector('#copy').innerHTML;
+    var elemPast = document.querySelector('#past');
+
+    elemPast.innerHTML += elemCopy;
+};
+
